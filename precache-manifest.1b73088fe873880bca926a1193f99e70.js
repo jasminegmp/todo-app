cee0e23447402b1f7d1746e776139df75b013ef7
jasminegmp <jasminekgump@gmail.com>
2019-06-10 17:47:47 -0700

    Updates

---
+++ precache-manifest.1b73088fe873880bca926a1193f99e70.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7598e2621ed0ad53ea50afce06f4ad14",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/todo-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "97d89e8248a2653457ec",
+    "url": "/todo-app/static/js/main.84695013.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
---
+++ static/js/main.84695013.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},9:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),o=t(2);var l=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null,"Hello World!"))};t.n(o).a.render(a.a.createElement(l,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.84695013.chunk.js.map
